952e5c6cfd277bcef0280f48276cab03afa09900
José Fernández Alameda <jose@ally.se>
2025-12-25 16:43:51 +0100

    Native function macro.

--- src/interpreter/methods/math.rs
+++ src/interpreter/methods/math.rs
@@ -2,24 +2,25 @@ use std::rc::Rc;
 
 use crate::{
     interpreter::{
-        runtime_errors::RuntimeError,
-        value::{Convert, Value},
+        methods::{NativeFnArgs, NativeFnReturn}, runtime_errors::RuntimeError, value::{Convert, Value}
     },
-    register_method,
+    register_method, takes_arguments,
 };
 
-pub fn fn_sin(args: Vec<Rc<Value>>) -> Result<Rc<Value>, RuntimeError> {
-    let number = args.first().unwrap();
-    let number = f64::convert(number.to_number()).unwrap();
+pub fn fn_sin(args: NativeFnArgs) -> Result<NativeFnReturn, RuntimeError> {
+    let (angle,) = takes_arguments!(args, 1)?;
+
+    let number = f64::convert(angle.to_number()).unwrap();
+    
     Ok(Rc::new(Value::Float(f64::sin(number))))
 }
 
-pub fn fn_cos(args: Vec<Rc<Value>>) -> Result<Rc<Value>, RuntimeError> {
-    let value = args.first().unwrap();
+pub fn fn_cos(args: NativeFnArgs) -> Result<NativeFnReturn, RuntimeError> {
+    let (angle,) = takes_arguments!(args, 1)?;
 
     // Convert anything to f64 using your existing logic
     // TODO: Implement proper runtime error handing
-    let number = value.to_f64();
+    let number = angle.to_f64();
 
     Ok(Rc::new(Value::Float(number.cos())))
 }
--- src/interpreter/methods/mod.rs
+++ src/interpreter/methods/mod.rs
@@ -17,7 +17,10 @@ pub struct Method {
 
 inventory::collect!(Method);
 
-pub fn get_method(name: String, args: Vec<Rc<Value>>) -> Result<Rc<Value>, RuntimeError> {
+type NativeFnArgs = Vec<Rc<Value>>;
+type NativeFnReturn = Rc<Value>;
+
+pub fn get_method(name: String, args: NativeFnArgs) -> Result<NativeFnReturn, RuntimeError> {
     for method in inventory::iter::<Method> {
         if method.name == name {
             return (method.func)(args);
@@ -26,6 +29,71 @@ pub fn get_method(name: String, args: Vec<Rc<Value>>) -> Result<Rc<Value>, Runti
 
     Err(RuntimeError::new(format!("Method not found: {}", name)))
 }
+
+#[macro_export]
+macro_rules! takes_arguments {
+    ($args:expr, 0) => {{
+        if $args.len() != 0 {
+            return Err(RuntimeError::new(format!(
+                "Expected 0 parameters, found {}",
+                $args.len()
+            )));
+        }
+        Ok(())
+    }};
+    ($args:expr, 1) => {{
+        if $args.len() != 1 {
+            return Err(RuntimeError::new(format!(
+                "Expected 1 parameter, found {}",
+                $args.len()
+            )));
+        }
+        Ok(($args[0].clone(),))
+    }};
+    ($args:expr, 2) => {{
+        if $args.len() != 2 {
+            return Err(RuntimeError::new(format!(
+                "Expected 2 parameters, found {}",
+                $args.len()
+            )));
+        }
+        Ok(($args[0].clone(), $args[1].clone()))
+    }};
+    ($args:expr, 3) => {{
+        if $args.len() != 3 {
+            return Err(RuntimeError::new(format!(
+                "Expected 3 parameters, found {}",
+                $args.len()
+            )));
+        }
+        Ok(($args[0].clone(), $args[1].clone(), $args[2].clone()))
+    }};
+    ($args:expr, 4) => {{
+        if $args.len() != 4 {
+            return Err(RuntimeError::new(format!(
+                "Expected 4 parameters, found {}",
+                $args.len()
+            )));
+        }
+        Ok(($args[0].clone(), $args[1].clone(), $args[2].clone(), $args[3].clone()))
+    }};
+    ($args:expr, 5) => {{
+        if $args.len() != 5 {
+            return Err(RuntimeError::new(format!(
+                "Expected 5 parameters, found {}",
+                $args.len()
+            )));
+        }
+        Ok((
+            $args[0].clone(),
+            $args[1].clone(),
+            $args[2].clone(),
+            $args[3].clone(),
+            $args[4].clone(),
+        ))
+    }};
+}
+
 
 #[macro_export]
 macro_rules! register_method {
